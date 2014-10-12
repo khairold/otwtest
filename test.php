ed3e01335751f4308472058cb843c22984ec1fd9
Khairold Safri Ibrahim <khairold@gmail.com>
2014-10-12 22:29:16 +0800

    test php
    1st file

---
+++ test.php
@@ -0,0 +1,380 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Welcome to Nasi Minyak</title>
+  <meta charset="utf-8" />
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+  <meta name="referrer" content="always">
+  <!--[if gte IE 7]><!-->
+  <link href="https://d13yacurqjgara.cloudfront.net/assets/master-6ad1d53524e36b3303c5bd6b342d6629.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  
+  <link href="https://d13yacurqjgara.cloudfront.net/assets/adapt-b650c16c901fe4c639465c210c158b48.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <!-- <![endif]-->
+  <link href="https://d13yacurqjgara.cloudfront.net/assets/print-5ae5449b45148c0f05877c48fdc92c13.css" media="print" rel="stylesheet" type="text/css" />
+  <link href="https://d13yacurqjgara.cloudfront.net/assets/apple-touch-icon-precomposed-149422efdbc7dba50c50ff0349f1a4dc.png" rel="apple-touch-icon-precomposed" />
+  <link href="https://cdn0.iconfinder.com/data/icons/huge-business-icons/512/Wedding.png" rel="icon" />
+  
+  
+
+  
+</head>
+
+<body id="shots" class="logged-out not-pro not-self not-team">
+
+<div id="header" class="group"><div id="header-inner" class="group">
+
+  
+
+  <a href="#nav" id="toggle-nav">Toggle navigation</a>
+
+  <ul id="nav">
+    <li id="t-search">
+      <form id="search" action="https://nasiminyak.com/search">
+        <input class="search-text" type="text" name="q" placeholder="Search " value="" />
+        
+      </form>
+    </li>
+    <li id="t-signin">
+      <a href="https://nasiminnyak.com/session/new">
+        <span>Sign in</span>
+</a>    </li>
+    <li id="t-signup">
+      <a href="https://nasiminyak.com/signup">Sign up</a>
+    </li>
+    <li id="t-shots">
+      <a href="/shots" class="has-sub">menu 1</a>
+      <ul class="tabs">
+  <li class=""><a href="/shots">1</a></li>
+  <li class=""><a href="/shots?sort=recent">2</a></li>
+  <li class=""><a href="/shots?list=teams">3</a></li>
+  <li class=""><a href="/shots?list=debuts">4</a></li>
+  <li class=""><a href="/shots?list=playoffs">5</a></li>
+  <li class=""><a href="/shots?list=animated">6</a></li>
+  
+</ul>
+
+    </li>
+    <li id="t-players">
+      <a href="/designers" class="has-sub">menu 2</a>
+      <ul class="tabs">
+  <li class=""><a href="/designers">1</a></li>
+  <li class=""><a href="/designers?for_hire=on">2</a></li>
+  <li class="separate"><a href="/designers/overview">3</a></li>
+  <li class=""><a href="/skills">4</a></li>
+  <li class=""><a href="/cities">5</a></li>
+  <li class=""><a href="/countries">6</a></li>
+  <li class=""><a href="/designers/prospects">7</a></li>
+</ul>
+
+   
+    <li id="t-more">
+      <a href="/highlights">
+        <span>More</span>
+</a>      <ul class="tabs">
+        <li><li class=""><a href="/highlights">Highlights</a></li></li>
+        <li><li class=""><a href="/projects">Projects</a></li></li>
+        <li><li class=""><a href="/buckets">Buckets</a></li></li>
+        <li><li class=""><a href="/colors">Colors</a></li></li>
+        <li><li class=""><a href="/tags">Tags</a></li></li>
+        <li class="separate"><a href="/about">About</a></li>
+        <li><a href="http://nasiminyak.com">Blog</a></li>
+        <li><a href="http://equipment.dribbble.com/">Shop</a></li>
+        <li><a href="/meetups">Meetups</a></li>
+      </ul>
+    </li>
+  </ul>
+</div></div> <!-- /header -->
+
+<hr />
+
+<div class="ajax notice hide">
+  <h2></h2>
+</div>
+
+
+
+
+<div id="wrap">
+
+
+
+<div id="wrap-inner">
+
+<div id="content" class="group">
+
+
+
+<div id="main" class="main-full">
+  
+<div class="full full-pitch group">
+  <h1 class="compact">
+    <strong>How was Your wedding? Tell us!</strong>
+    Nasiminyak.com is the right place!
+  </h1>
+</div>
+
+        <ol class="dribbbles group">
+    <li id="screenshot-1760457" class="group ">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760457-Steampunk-violin" class="dribbble-link"><div data-picture data-alt="Steampunk violin">
+  <div data-src="http://www.midtownloft.net/photos/weddings/wedding-venue-16.jpg"></div>
+  
+  
+</div>
+</a>
+  <a href="/shots/1760457-Steampunk-violin" class="dribbble-over">    <strong>Venues</strong>
+      <span class="comment">Update us the where was your wedding held...</span>
+
+    <em class="timestamp"></em>
+</a>
+
+ 
+</li>
+<li id="screenshot-1760627" class="group ">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760627-Deck" class="dribbble-link"><div data-picture data-alt="Deck">
+  <div data-src="http://www.tajweddingservices.co.uk/images2/Catering/catering_taj_wedding_services.jpg"></div>
+ 
+  
+</div>
+</a>
+  <a href="/shots/1760627-Deck" class="dribbble-over">    <strong>Caterer</strong>
+      <span class="comment">Update us about your wedding caterer...nice???</span>
+
+    <em class="timestamp"></em>
+</a>
+</a>
+    </span>
+  </h2>
+</li>
+<li id="screenshot-1760693" class="group ">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760693-Google-Primer" class="dribbble-link"><div data-picture data-alt="Google Primer">
+  <div data-src="http://upload.wikimedia.org/wikipedia/en/c/cd/Majlis_ambil-ambilan.jpg"></div>
+  
+  <noscript>
+    <img alt="Google Primer" src="https://d13yacurqjgara.cloudfront.net/users/45889/screenshots/1760693/google_primer_teaser.jpg" />
+  </noscript>
+</div>
+</a>
+  <a href="/shots/1760693-Google-Primer" class="dribbble-over">    <strong>Wedding Dress</strong>
+      <span class="comment">Desribe Your Wedding Dress!</span>
+
+    <em class="timestamp"></em>
+</a>
+   
+
+</a>
+    </span>
+  </h2>
+</li>
+<li id="screenshot-1760273" class="group ">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760273-Ramotion-Morphing-GIF" class="dribbble-link"><div data-picture data-alt="Ramotion Morphing [GIF]">
+  <div data-src="http://www.exclusiveitalyweddings.com/blog/wp-content/uploads/2012/04/Hiring-Bridal-Make-Up-Artists.jpg"></div>
+
+  
+</div>
+</a>
+  <a href="/shots/1760273-Ramotion-Morphing-GIF" class="dribbble-over">    <strong>Make-Up Artist</strong>
+      <span class="comment">Who is the make up artist? Do you like it?</span>
+
+    <em class="timestamp"></em>
+</a>
+</a>
+    </span>
+  </h2>
+</li>
+<li id="screenshot-1760231" class="group ">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760231-Little-Carl" class="dribbble-link"><div data-picture data-alt="Little Carl ">
+  <div data-src="http://www.pbts.com/wp-content/uploads/2013/06/booking-Photographer.jpg"></div>
+  
+  
+</div>
+</a>
+  <a href="/shots/1760231-Little-Carl" class="dribbble-over">    <strong>Photographer</strong>
+      <span class="comment">Do you enjoyed your photography session? Tell us! </span>
+
+    <em class="timestamp"></em>
+</a>
+</li>
+<li id="screenshot-1760361" class="group team">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760361-Papers" class="dribbble-link"><div data-picture data-alt="Papers">
+  <div data-src="http://projectc1.projectcasting.netdna-cdn.com/wp-content/uploads/2013/04/los-angeles-videographer.jpg"></div>
+
+  
+</div>
+</a>
+  <a href="/shots/1760361-Papers" class="dribbble-over">    <strong>Videographer</strong>
+      <span class="comment">How was the video?</span>
+
+    <em class="timestamp"></em>
+</a>
+</a>
+</a>
+    </span>
+  </h2>
+</li>
+<li id="screenshot-1760237" class="group ">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760237-Ladybird" class="dribbble-link"><div data-picture data-alt="Ladybird">
+  <div data-src="http://kahwinmall.com/images/ad_img_thumb/7480_2013-07-13-05-49-49-gambar-gubahan-hantaran-gambar-kahwin-gambar-pelamin-gambar-pengantin-kahwin-mall.jpg"></div>
+ 
+  
+</div>
+</a>
+  <a href="/shots/1760237-Ladybird" class="dribbble-over">    <strong>Doorgifts</strong>
+      <span class="comment">describe the doorgifts....</span>
+
+    <em class="timestamp"></em>
+</a>
+</a>
+    </span>
+  </h2>
+</li>
+<li id="screenshot-1760319" class="group team">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760319-Profile-Cards-Concepts" class="dribbble-link"><div data-picture data-alt="Profile Cards - Concepts">
+  <div data-src="http://www.bollywoodvibes.co.uk/img/p-wedding.jpg"></div>
+  
+  <noscript>
+    <img alt="Profile Cards - Concepts" src="https://d13yacurqjgara.cloudfront.net/users/104062/screenshots/1760319/untitled-1_teaser.png" />
+  </noscript>
+</div>
+</a>
+  <a href="/shots/1760319-Profile-Cards-Concepts" class="dribbble-over">    <strong>Stage Performance</strong>
+	<span class="comment">describe the stage performance...</span>
+    <em class="timestamp"></em>
+</a>
+    
+
+</a>
+    </span>
+  </h2>
+</li>
+<li id="screenshot-1760571" class="group ">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760571-Camp-Brand-Goods-winter-tee" class="dribbble-link"><div data-picture data-alt="Camp Brand Goods winter tee">
+  <div data-src="http://www.mmmjewelry.com/images/rings/1.jpg"></div>
+  
+  
+</div>
+</a>
+  <a href="/shots/1760571-Camp-Brand-Goods-winter-tee" class="dribbble-over">    <strong>Wedding rings.</strong>
+	<span class="comment">wedding rings....tell us bout it more!</span>
+    <em class="timestamp"></em>
+</a>
+
+</a>
+    </span>
+  </h2>
+</li>
+<li id="screenshot-1760355" class="group ">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  <a href="/shots/1760355-Battery" class="dribbble-link"><div data-picture data-alt="Battery">
+  <div data-src="http://api.ning.com/files/kF*sijNveAcHRNyee7MsMh15kgq7ghbMjEBSlgPKmhDPZz2CYTztoBt0Bb2v2hSRLOuj-gHvOcs29m4RHuPKf9Qoc6zqSC93/brghantaran.jpg"></div>
+ 
+  
+</div>
+</a>
+  <a href="/shots/1760355-Battery" class="dribbble-over">    <strong>Wedding Dowries</strong>
+      <span class="comment">what are the dowries you get?</span>
+
+    <em class="timestamp"></em>
+</a>
+</a>
+    </span>
+  </h2>
+  <div>
+</li>
+
+<li id="screenshot-1760156" class="group team">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+   </div>
+
+ 
+
+    </span>
+  </h2>
+</li>
+<li id="screenshot-1760635" class="group team">
+  <div class="dribbble">
+    <div class="dribbble-shot">
+      <div class="dribbble-img">
+  
+</div>
+
+
+   
+
+  </div>
+
+  
+</li>
+
+  </ol>
+
+
+</div> <!-- /main -->
+
+
+
+<hr />
+
+
+
+  <p class="follow">
+    <a href="http://twitter.com/nasiminyak"><img alt="Icon-team-twitter" src="https://d13yacurqjgara.cloudfront.net/assets/icon-team-twitter-225d7c7bbf69a2e84d91f484d78c689f.png" width="24" /></a>
+    <a href="http://www.facebook.com/nasiminyakcom"><img alt="Icon-team-facebook" src="https://d13yacurqjgara.cloudfront.net/assets/icon-team-facebook-a9ed2573a6c0130360de20ba1e0fedf8.png" width="24" /></a>
+    <a href="http://instagram.com/nasiminyak"><img alt="Icon-team-instagram" src="https://d13yacurqjgara.cloudfront.net/assets/icon-team-instagram-77614abc74782b09aa717d8d1762b87e.png" width="24" /></a>
+    <a href="http://blog.nasiminyak.com"><img alt="Icon-team-tumblr" src="https://d13yacurqjgara.cloudfront.net/assets/icon-team-tumblr-bee52e5eed7c8fbfca812283c7420dcd.png" width="24" /></a>
+  </p>
+</div></div> <!-- /footer -->
+
+
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+<script src="https://d13yacurqjgara.cloudfront.net/assets/global-bfcb4207cf253fce9a67a5a7d84e1830.js" type="text/javascript"></script>
+
+
+<div class="thedeck loading-dock hide">
+<script>
+var $deckAdContainer = $('.ad.thedeck.target');
+if ($deckAdContainer.length) {
+  var id = "DB";
+  var scriptLink = '<script type="text/javascript" src="https://connect.decknetwork.net/deck' + id + '_js.php?' + (new Date().getTime()) + '"></' + 'script>';
+  document.write(scriptLink);
+}
+</script>
+
+
+
+</body>
+</html>
